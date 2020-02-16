97e3a2134fe6a78c596f803ddd7bf7e417774f19
Jimmy Blundell <jblundell123@gmail.com>
2020-02-16 10:42:32 -0600

    Initial Commit

---
+++ Schedule Generator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Schedule Generator
+//
+//  Created by Jimmy Blundell on 2/16/20.
+//  Copyright © 2020 Jimmy Blundell. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
